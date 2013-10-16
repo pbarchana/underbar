f16710ac76110420a15f57ec9bdc5f50dec1c733
pbarchana <pb_archana@yahoo.com>
2013-10-16 01:35:51 -0700

    function reject works

--- src/underbar.js
+++ src/underbar.js
@@ -91,6 +91,9 @@ var _ = { };
   _.reject = function(collection, iterator) {
     // TIP: see if you can re-use _.select() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(x){
+      return !iterator(x);
+    });
   };
 
   // Produce a duplicate-free version of the array.
